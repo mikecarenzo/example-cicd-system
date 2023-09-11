c905808af7c53bc4de6879f5d701b817b7d5f1dc
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2023-09-11 19:54:56 -0400

    add initial cli tools

---
+++ src/cli/schematools/validator/RegexValidator.py
@@ -0,0 +1,26 @@
+import re
+
+from .Validator import Validator
+
+
+class RegexValidator(Validator):
+    def __init__(self):
+        """
+        Creates a new RegexValidator.
+        """
+        self.__validator = r"^\d{4}\s+\D+$"
+
+    def validate(self, string):
+        """
+        Tests if a string is valid.
+
+        Args:
+            string (str): The string to test.
+
+        Returns:
+            bool: True if the string is valid, false otherwise.
+        """
+        if isinstance(string, str):
+            return re.match(self.__validator, string) is not None
+        else:
+            raise TypeError("Expected string.")
